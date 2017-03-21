2a659f4f369b3dc60f69d111d3a5526c4d887cbc
Kevin Zeng <zengfenfei@gmail.com>
2017-03-21 19:52:29 +0800

    Insert call logs into db

--- src/export-pg.ts
+++ src/export-pg.ts
@@ -12,14 +12,42 @@ async function main() {
 	});
 
 	let pgPool = new pg.Pool(config.pg);
-	pgPool.connect((err, pgClient, done) => {
-		if (err) {
-			return console.error('Error fetching client from conneciton pool', err);
-		}
-		console.log('Connected to PG');
-		done();
-	});
 	pgPool.on('error', e => {
 		console.error('PG database error', e);
 	});
+	let dateFrom = new Date();
+	dateFrom.setFullYear(2015);
+	let callLogs = await rc.account().callLog().list({ view: 'Simple', dateFrom: dateFrom.toISOString() });
+	for (let callLog of callLogs.records) {
+		await insertObject(pgPool, 'rc_call_log', {
+			id: callLog.id,
+			sessionId: callLog.sessionId,
+			startTime: callLog.startTime,
+			duration: callLog.duration,
+			direction: callLog.direction,
+			action: callLog.action,
+			result: callLog.result,
+			toNumber: callLog.to && callLog.to.phoneNumber,
+			fromNumber: callLog.from && callLog.from.phoneNumber
+		});
+	}
+	pgPool.end();
+	console.log('Done.');
+}
+
+async function insertObject(pgPool, tableName, record) {
+	let fldNames = [];
+	let valueHolders = [];
+	let values = [];
+	for (let p in record) {
+		fldNames.push(p);
+		valueHolders.push('$' + fldNames.length);
+		values.push(record[p]);
+	}
+	let sqlText = `insert into ${tableName}(${fldNames.join(',')}) values(${valueHolders.join(',')})`;
+	try {
+		return await pgPool.query(sqlText, values);
+	} catch (e) {
+		console.error('Insert error', e);
+	}
 }
